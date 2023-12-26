f7eafabdc1f58065ac2b1068789597fd873fc2fb
Joseph Morales Silva <josephzx1@hotmail.com>
2023-12-26 09:17:41 -0600

    Add comment explaining app component selector

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
+//Por aca es como arranca la aplicación
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
