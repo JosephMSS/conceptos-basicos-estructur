02edc1bf1029cf7407a9432d10e8bf56e23ac638
Joseph Morales Silva <josephzx1@hotmail.com>
2023-12-26 09:17:32 -0600

    Add change detection strategy and import DashboardComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DashboardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'conceptos-basicos-estructur';
+  title = 'conceptos-básicos-estructura';
 }
